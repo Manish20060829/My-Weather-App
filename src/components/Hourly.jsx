ab80094f650828afbae7b651a1001aa846ec12e0
Manish20060829 <shotgunfanboy@gmail.com>
2025-07-29 21:22:51 -0400

    Hourly feature being added

--- src/components/Hourly.jsx
+++ src/components/Hourly.jsx
@@ -6,67 +6,40 @@ import Item from "./Item"
 
 function Hourly({ weatherdata }){
 
-const [ time , settime ] = useState([]);
-const [ index , setIndex ] = useState(0);
-
-useEffect(() => { 
-
-let newarray = weatherdata?.hourly?.time?.map((value) => {
-
-  return value.split("T")
-
- 
-
-})
 
 
 
+useEffect(() => {
+
+  const displaytimearray = [
+  "12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am",
+  "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"
+];
+
+const militarytimearray = [
+  "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
+  "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"
+];
+  let array = [];
+
+  for( let i = 1; i < 6; i++){
+
+    let time = weatherdata?.hourly?.time[i]
+    let timesplit = time?.split("T")
+    let index = militarytimearray.findIndex((value) => {
+
+      return (value === timesplit?.[1])
+
+    })
+
+    let displaytime = displaytimearray[index]
+    console.log(displaytime)
+
+
+    
+
+  }
 }, [weatherdata])
-
-useEffect(() => {
-let d = new Date();
-let hours = d.getHours();
-
-let month = (d.getMonth() + 1)
-
-if(month < 10) {
-
-month = "0" + month;
-
-}
-
-
-if(hours < 10){
-  hours = "0" + hours + ":00"
-}
-
-else{
-  hours = hours + ":00"
-
-}
-
-
-
-let date = d.getFullYear() + "-" + month + "-" + d.getDate();
-
-console.log(date)
-console.log(hours)
-
-const num = time.findIndex((value) => { 
-
-  return (value[0].equals(date) && value[1].equals(hours))
-  
-
-})
-
-setIndex(num);
-
-
-}, [time])
-
-useEffect(() => {
-  console.log(index)
-}, [index])
 
 
 
